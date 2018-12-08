257226e6ba5e24e4780c821f4465b1002e1eba92
SplinterDev <fabriciojcmontenegro@gmail.com>
2018-12-08 13:23:06 -0200

    2/3 files revised. arm.py seems alright now.

--- arm.py
+++ arm.py
@@ -1,12 +1,12 @@
-from point2 import Point2
-import numpy as np
-from helper import scale
+from point2d import Point2D
+import math
 
-MAX_THETA = np.pi
-MIN_THETA = -np.pi
+PI = math.pi
+MAX_THETA = PI
+MIN_THETA = -PI
 
 class Arm:
-    def __init__(self, l1=200, l2=200, l3=200, t1=0.0, t2=0., t3=np.pi/2.):
+    def __init__(self, l1=200, l2=200, l3=200, t1=0.0, t2=0., t3=PI/2.):
         self.t1 = t1
         self.t2 = t2
         self.t3 = t3
@@ -20,25 +20,31 @@ class Arm:
         self.t3 += dt3
 
         if self.t1 > MAX_THETA:
-            self.t1 -= 2*np.pi
+            self.t1 -= 2*PI
         if self.t2 > MAX_THETA:
-            self.t2 -= 2*np.pi
+            self.t2 -= 2*PI
         if self.t3 > MAX_THETA:
-            self.t3 -= 2*np.pi
+            self.t3 -= 2*PI
 
         if self.t1 < MIN_THETA:
-            self.t1 += 2*np.pi
+            self.t1 += 2*PI
         if self.t2 < MIN_THETA:
-            self.t2 += 2*np.pi
+            self.t2 += 2*PI
         if self.t3 < MIN_THETA:
-            self.t3 += 2*np.pi
+            self.t3 += 2*PI
 
     def endeffector(self):
-        p1 = Point2().polar(self.l1, self.t1)
-        p2 = Point2().polar(self.l2, self.t2+self.t1) + p1
-        return Point2().polar(self.l3, self.t3+self.t2+self.t1) + p2
+        p1 = Point2D(r=self.l1, a=self.t1)
+        p2 = Point2D(r=self.l2, a=self.t2+self.t1) + p1
+        return Point2D(r=self.l3, a=self.t3+self.t2+self.t1) + p2
+
+if __name__ == '__main__':
+    a = Arm()
+    print(a.endeffector())
 
 
+    # THIS CODE WAS TAKEN FROM INSIDE THE CLASS
+    # IM GONNA JUDGE IF IT'S NEEDED AT ANOTHER TIME
     # def set_random_pose(self):
     #     t1 = scale(0.0, 1.0, MIN_THETA, MAX_THETA, np.random.random())
     #     t2 = scale(0.0, 1.0, MIN_THETA, MAX_THETA, np.random.random())

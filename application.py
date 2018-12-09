2d1259114e3a57dfded52180cdcd619fd40e1b43
SplinterDev <fabriciojcmontenegro@gmail.com>
2018-12-09 18:16:37 -0200

    Cleaned up arm.py and made it usable for N-joints robots. Cleaned up application.py

--- application.py
+++ application.py
@@ -1,286 +1,70 @@
 from ezgame import Ezgame
 from point2d import Point2D
-# import numpy as np
-# import csv
 
 from arm import *
-from trajectory import *
-# from jacobian import JacobianControler
-# from net import Net
-# from helper import *
-# from logger import Logger
 
 import time
 
-FPS = 100000
+FPS = 30
 
 
 # APPLICATION MODES
-JACOBIAN            = 0
-RANDOM_POSE         = 1
-GENERATE_SAMPLES    = 2
-TEST_NETWORK        = 3
+JACOBIAN    = 0
+INCREMENTER = 1
 
-# MODELID = "__100_1024_Adam_sigmoid_640_320_160_0.009256326838229832_0.009620750007629394_0.000903887354967dataset_5_0_20.csv"
-mini = 0.1
-maxi = 0.9
-
-CURRENT_MODE = JACOBIAN
-
+CURRENT_MODE = INCREMENTER
+# to avoid creating a new Point2D as (0,0) every loop, reuse this one:
+ORIGIN = Point2D()
 
 class Application:
     def __init__(self):
-        self.ez = Ezgame(700, 700)
+        # creates an ezgame application with size (800x800)
+        # and sets its fps to FPS
+        self.ez = Ezgame(800, 800)
         self.ez.fps = FPS
+        # self.loop identifies what loop function will be used
         self.loop = CURRENT_MODE
 
-        self.arm = Arm(t2=math.pi/2.)
+        # for starters, creates 3 links and 3 joints
+        lengths = [200]*3
+        thetas  = [0, 0, math.pi/2.]
+        # and uses them to create an arm
+        self.arm = Arm(lengths, thetas)
         self.init()
 
-
     def init(self):
-        if self.loop == JACOBIAN:
-            self.ez.init(self.jacobian_loop)
-            # needs a trajectory
-            self.traj_step  = TRAJ_STEP_MIN
-            self.trajectory = Trajectory(
-                step_length=self.traj_step,
-                restart=self.reset)
-            # self.jacobian   = JacobianControler(self.arm)
-            self.errors = []
-            self.plot_errors = []
-            # self.jacobian_logger = Logger("jacobian_control")
+        # using self.loop, initializes a bunch of stuff
+        if self.loop == INCREMENTER:
+            self.ez.init(self.incrementer_loop)
             self.ez.gui = True
-
-        # elif self.loop == RANDOM_POSE:
-        #     self.ez.init(self.random_pose_loop)
-        #     self.arm2 = Arm()
-        #     self.ez.fps = 2
-
-        # elif self.loop == GENERATE_SAMPLES:
-        #     self.ez.init(self.generate_samples_loop)
-        #     self.arm2 = Arm()
-        #     self.ez.gui = False
-
-        #     # the file has in its name the range of delta theta in degrees
-        #     # and the maximum step valid
-        #     filename = "dataset_{}_{}-{}.csv".format(DT_VAL, MIN_STEP, MAX_STEP)
-        #     csvfile = open(filename, 'a')
-        #     self.dataset_writer  = csv.writer(csvfile)
-        #     self.sample_counter  = 0
-        #     self.invalid_counter = 0
-        #     self.fps = 1
-
-        # elif self.loop == TEST_NETWORK:
-        #     self.ez.init(self.test_net_loop)
-        #     self.ez.gui = True
-        #     # needs a trajectory
-        #     self.traj_step  = TRAJ_STEP_MIN
-        #     self.trajectory = Trajectory(
-        #         step_length=self.traj_step,
-        #         restart=self.reset)
-        #     modelid = MODELID
-        #     self.net = Net(modelid, self.arm)
-        #     self.errors = []
-        #     self.net_logger = Logger("poora{}".format(modelid))
-
-    # def test_net_loop(self):
-    #     self.draw_arm(self.arm)
-    #     self.draw_trajectory()
-
-    #     # get current position
-    #     # t1  = self.arm.t1
-    #     # t2  = self.arm.t2
-    #     # ee1 = self.arm.endeffector()
-
-    #     self.current_point = self.trajectory.current()
-    #     self.net.control(self.current_point, mini, maxi)
-    #     self.errors.append(self.arm.error(self.current_point))
-    #     self.draw_info()
-
-    #     self.current_point = self.trajectory.next()
-
-    #     self.ez.point(self.arm.endeffector(), color='blue')
-
-    def jacobian_loop(self):
-        self.draw_arm(self.arm)
-        self.draw_trajectory()
-
-        self.current_point = self.trajectory.current()
-        # self.jacobian.control(self.current_point)
-
-        self.errors.append(self.arm.error(self.current_point))
-        self.draw_info()
-
-        self.current_point = self.trajectory.next()
-
-        self.ez.point(self.arm.endeffector(), color='blue')
-
-        #time.sleep(0.1)
-
-    # def random_pose_loop(self):
-    #     self.arm.set_random_pose()
-    #     t1, t2, t3 = self.arm.get_normalized_pose()
-    #     self.arm2.set_normalized_pose(t1, t2, t3)
-    #     self.draw_arm(self.arm)
-    #     self.draw_arm(self.arm2)
-
-    # def generate_samples_loop(self):
-    #     sample = self.generate_sample()
-    #     vector = Point2(sample[3], sample[4])
-    #     if vector.r > MIN_STEP and vector.r < MAX_STEP:
-    #         norm_sample = self.normalize_sample(sample, mini, maxi)
-    #         self.dataset_writer.writerow(norm_sample)
-    #         self.sample_counter += 1
-    #     else:
-    #         self.invalid_counter += 1
-
-    #     if self.sample_counter % 10000 == 0:
-    #         print "{} samples generated in this loop.".format(self.sample_counter)
-    #         avg = self.sample_counter/float((self.sample_counter + self.invalid_counter))
-    #         print "{}% valid".format(avg*100.)
-
-    #     if self.sample_counter > 2000000:
-    #         self.exit()
-
-
-    # def generate_sample(self):
-    #     # generate random position
-    #     self.arm.set_random_pose()
-
-    #     #view
-    #     # self.draw_arm(self.arm, color='blue')
-    #     #endview
-
-    #     t1, t2, t3 = self.arm.t1, self.arm.t2, self.arm.t3
-    #     ee1 = self.arm.endeffector()
-
-    #     # generate random movement
-    #     dt1 = normal_in_range(MIN_DT, MAX_DT)
-    #     dt2 = normal_in_range(MIN_DT, MAX_DT)
-    #     dt3 = normal_in_range(MIN_DT, MAX_DT)
-    #     #dt1 = scale(0.0, 1.0, MIN_DT, MAX_DT, np.random.random())
-    #     #dt2 = scale(0.0, 1.0, MIN_DT, MAX_DT, np.random.random())
-    #     #dt3 = scale(0.0, 1.0, MIN_DT, MAX_DT, np.random.random())
-        
-    #     # move arm
-    #     self.arm.move(dt1, dt2, dt3)
-    #     ee2 = self.arm.endeffector()
-    #     dee = ee2 - ee1
-
-    #     #view
-    #     # self.arm2.t1, self.arm2.t2, self.arm2.t3 = self.arm.t1, self.arm.t2, self.arm.t3
-    #     # self.draw_arm(self.arm2, color='red')
-    #     # # sample = [t1,t2,t3, dee.x, dee.y, dt1, dt2, dt3]
-    #     # print "t1:{}\nt2:{}\nt3:{}\ndee.x:{}\ndee.y:{}\ndt1:{}\ndt2:{}\ndt3:{}".format(
-    #     #     np.degrees(t1),
-    #     #     np.degrees(t2),
-    #     #     np.degrees(t3),
-    #     #     dee.x, dee.y,
-    #     #     np.degrees(dt1),
-    #     #     np.degrees(dt2),
-    #     #     np.degrees(dt3)
-    #     # )
-    #     # print ee1, " ->", ee2
-    #     # # print [t1,t2,t3, dee.x, dee.y, dt1, dt2, dt3], ":", ee1, " ->", ee2
-    #     # raw_input()
-    #     #endview
-
-    #     return [t1,t2, t3, dee.x, dee.y, dt1, dt2, dt3]
-
-    # def normalize_sample(self, sample, mini=0.1, maxi=0.9):
-    #     t1  = scale(MIN_THETA, MAX_THETA, mini, maxi, sample[0])
-    #     t2  = scale(MIN_THETA, MAX_THETA, mini, maxi, sample[1])
-    #     t3  = scale(MIN_THETA, MAX_THETA, mini, maxi, sample[2])
-    #     dx  = scale(-MAX_STEP, MAX_STEP, mini, maxi, sample[3])
-    #     dy  = scale(-MAX_STEP, MAX_STEP, mini, maxi, sample[4])
-    #     dt1 = scale(MIN_DT, MAX_DT, mini, maxi, sample[5])
-    #     dt2 = scale(MIN_DT, MAX_DT, mini, maxi, sample[6])
-    #     dt3 = scale(MIN_DT, MAX_DT, mini, maxi, sample[7])
-
-    #     norm_sample = [t1, t2, t3, dx, dy, dt1, dt2, dt3]
-    #     for value in norm_sample:
-    #         assert value >= 0.0 and value <= 1.0
-    #     return norm_sample
-
-
-    def draw_arm(self, arm, color='black'):
-        p1 = Point2D(r=arm.l1, a=arm.t1)
-        p2 = Point2D(r=arm.l2, a=arm.t2+arm.t1) + p1
-        p3 = Point2D(r=arm.l3, a=arm.t3+arm.t2+arm.t1) + p2
-
-        self.ez.line(Point2D(), p1, color=color)
-        self.ez.point(p1)
-        self.ez.line(p1, p2, color=color)
-        self.ez.point(p2)
-        self.ez.line(p2, p3, color=color)
-        self.ez.point(p3)
-
-    def draw_trajectory(self):
-        self.ez.lines(self.trajectory.points)
-
-    def draw_info(self):
-        # tamanho do passo atual
-        val = self.traj_step
-        val_text = "Step size.....{0}".format(val)
-        self.ez.text(val_text, Point2D(10,10), onScreen=True)
-        # passo atual
-        val = self.trajectory.curr_idx()
-        val_text = "Current step..{0}".format(val)
-        self.ez.text(val_text, Point2D(10,20), onScreen=True)
-        # erro medio
-        val = self.mse()
-        val_text = "Avg. error....{0}".format(val)
-        self.ez.text(val_text, Point2D(10,30), onScreen=True)
-
-        if self.loop == JACOBIAN:
-            # temporary
-            if len(self.plot_errors) > 2:
-                self.ez.lines(self.plot_errors,color='blue')
-
-    def mse(self):
-        squared = [e*e for e in self.errors]
-        # squared = [e for e in self.errors]
-        return sum(squared)/float(len(squared))
-
-    def run(self):
-        self.ez.run()
-
-    def reset(self):
-        mse = self.mse()
-        # print self.traj_step, mse
-
-        # if self.loop == TEST_NETWORK:
-        #     self.net_logger.add_data(self.traj_step, mse)
-        # elif self.loop == JACOBIAN:
-        #     self.jacobian_logger.add_data(self.traj_step, mse)
-
-
-        self.traj_step += TRAJ_STEP_INC
-        if self.traj_step > TRAJ_STEP_MAX:
+        else:
             self.exit()
 
-        self.trajectory = Trajectory(
-            step_length=self.traj_step,
-            restart=self.reset)
-        self.errors = []
-        self.arm = Arm()
+    def incrementer_loop(self):
+        # in this mode, we draw the arm
+        self.draw_arm(self.arm)
+        # paint the end-effector blue
+        self.ez.point(self.arm.endeffector(), color='blue')
+        # and move every joint in 1 degree
+        self.arm.move([math.pi/180.]*3)
 
-        if self.loop == JACOBIAN:
-            self.jacobian.arm = self.arm
-            # temporary
-            self.plot_errors.append(
-                Point2D(self.traj_step * 10, mse*1000)
-            )
-        elif self.loop == TEST_NETWORK:
-            self.net.arm = self.arm
+    def draw_arm(self, arm, color='black'):
+        # to draw the arm we get the position of each joint
+        points = arm.joints_pos()
+        for i, p in enumerate(points):
+            # links joints with lines
+            if i == 0:
+                self.ez.line(ORIGIN, p, color=color)
+            else:
+                self.ez.line(points[i-1], p, color=color)
+            # draws the joint as a point
+            self.ez.point(p)
+
+    def run(self):
+        # this only calls the ezgame run() method, dont worry about it
+        self.ez.run()
 
     def exit(self):
-        # if self.loop == TEST_NETWORK:
-        #     self.net_logger.plot("Step Size", "MSE")
-        # if self.loop == JACOBIAN:
-        #     self.jacobian_logger.plot("Step Size", "MSE")
-        raw_input()
         exit()
 
 
--- arm.py
+++ arm.py
@@ -6,83 +6,65 @@ MAX_THETA = PI
 MIN_THETA = -PI
 
 class Arm:
-    def __init__(self, l1=200, l2=200, l3=200, t1=0.0, t2=0., t3=PI/2.):
-        self.t1 = t1
-        self.t2 = t2
-        self.t3 = t3
-        self.l1 = l1
-        self.l2 = l2
-        self.l3 = l3
+    def __init__(self, lengths=[200, 200], thetas=[0, PI/2.]):
+        """
+        Receives:
+            lengths: list of links' lengths (default: 2 links of 200mm)
+            thetas: list of initial angles of joints (default: 2 joints -> 0 and pi/2)
 
-    def move(self, dt1, dt2, dt3):
-        self.t1 += dt1
-        self.t2 += dt2
-        self.t3 += dt3
+        lengths and thetas should be lists of the same size
+        """
+        if not len(lengths) == len(thetas):
+            raise Exception("Arm(): lengths and thetas should be lists of same size.")
+        self.lengths = lengths
+        self.thetas = thetas
+        self.num_joints = len(thetas)
 
-        if self.t1 > MAX_THETA:
-            self.t1 -= 2*PI
-        if self.t2 > MAX_THETA:
-            self.t2 -= 2*PI
-        if self.t3 > MAX_THETA:
-            self.t3 -= 2*PI
-
-        if self.t1 < MIN_THETA:
-            self.t1 += 2*PI
-        if self.t2 < MIN_THETA:
-            self.t2 += 2*PI
-        if self.t3 < MIN_THETA:
-            self.t3 += 2*PI
+    def move(self, dt):
+        """
+        Receives:
+            dt: list of delta theta changes in joint values
+        """
+        if not len(dt) == self.num_joints:
+            raise Exception("Arm.move(dt): dt, i.e, Delta theta, should have {} elements to move this arm.".format(self.num_joints))
+        for i, delta in enumerate(dt):
+            # moves it
+            self.thetas[i] += delta
+            # keeps its value between -pi and pi
+            if self.thetas[i] > MAX_THETA:
+                self.thetas[i] -= 2*PI
+            elif self.thetas[i] < MIN_THETA:
+                self.thetas[i] += 2*PI
 
     def endeffector(self):
-        p1 = Point2D(r=self.l1, a=self.t1)
-        p2 = Point2D(r=self.l2, a=self.t2+self.t1) + p1
-        return Point2D(r=self.l3, a=self.t3+self.t2+self.t1) + p2
+        """
+        Finds end-effector position through forward kinematics and returns it
+        """
+        
+        return self.joints_pos()[-1]
+
+    def joints_pos(self):
+        """
+        Returns the position of every joint in the space (useful for drawing them)
+        """
+        points = [Point2D() for i in self.thetas]
+
+        for i, p in enumerate(points):
+            p.r = self.lengths[i]
+            p.a = sum(self.thetas[:i+1])
+            if i > 0:
+                p += points[i-1]
+        return points
         
     def error(self, target):
-        # returns the euclidian distance between the endeffector and the target
+        """
+        Returns the euclidian distance between the endeffector and the target
+        """
         return (self.endeffector() - target).r
 
 if __name__ == '__main__':
     a = Arm()
     print(a.endeffector())
 
-
-    # THIS CODE WAS TAKEN FROM INSIDE THE CLASS
-    # IM GONNA JUDGE IF IT'S NEEDED AT ANOTHER TIME
-    # def set_random_pose(self):
-    #     t1 = scale(0.0, 1.0, MIN_THETA, MAX_THETA, np.random.random())
-    #     t2 = scale(0.0, 1.0, MIN_THETA, MAX_THETA, np.random.random())
-    #     t3 = scale(0.0, 1.0, MIN_THETA, MAX_THETA, np.random.random())
-    #     # check if the values are between MIN_THETA and MAX_THETA
-    #     assert t1 > MIN_THETA and t1 < MAX_THETA
-    #     assert t2 > MIN_THETA and t2 < MAX_THETA
-    #     assert t3 > MIN_THETA and t3 < MAX_THETA
-    #     # if they are, set joints to the values
-    #     self.t1 = t1
-    #     self.t2 = t2
-    #     self.t3 = t3
-
-    # def get_normalized_pose(self):
-    #     t1 = scale(MIN_THETA, MAX_THETA, 0.0, 1.0, self.t1)
-    #     t2 = scale(MIN_THETA, MAX_THETA, 0.0, 1.0, self.t2)
-    #     t3 = scale(MIN_THETA, MAX_THETA, 0.0, 1.0, self.t3)
-    #     # check if the values are between 0.0 and 1.0
-    #     assert t1 > 0.0 and t1 < 1.0
-    #     assert t2 > 0.0 and t2 < 1.0
-    #     assert t3 > 0.0 and t3 < 1.0
-    #     return t1, t2, t3
-
-    # def set_normalized_pose(self, t1, t2, t3):
-    #     t1 = scale(0.0, 1.0, MIN_THETA, MAX_THETA, t1)
-    #     t2 = scale(0.0, 1.0, MIN_THETA, MAX_THETA, t2)
-    #     t3 = scale(0.0, 1.0, MIN_THETA, MAX_THETA, t3)
-    #     # check if the values are between MIN_THETA and MAX_THETA
-    #     assert t1 > MIN_THETA and t1 < MAX_THETA
-    #     assert t2 > MIN_THETA and t2 < MAX_THETA
-    #     assert t3 > MIN_THETA and t3 < MAX_THETA
-    #     # if they are, set joints to the values
-    #     self.t1 = t1
-    #     self.t2 = t2
-    #     self.t3 = t3
 
 
